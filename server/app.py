c0b2f245718a3bdc15c778b7878142b95ced349d
maryam <121059049+maryamabdallahhh@users.noreply.github.com>
2025-04-26 00:04:06 +0300

    Hue Rotation

--- server/app.py
+++ server/app.py
@@ -64,6 +64,13 @@ def get_available_effects():
             {'id': 'factor', 'name': 'Brightness Factor', 'type': 'slider', 'min': 0, 'max': 2, 'step': 0.1, 'default': 1.0}
         ]
     },
+       'hue_rotation': {
+        'name': 'Hue Rotation',
+        'description': 'Rotate the hues of the image',
+        'params': [
+            {'id': 'angle', 'name': 'Angle', 'type': 'slider', 'min': -180, 'max': 180, 'step': 1, 'default': 0}
+        ]
+    },
     }
     
     available_effects = {k: v for k, v in effect_info.items() if k in EFFECTS}
---
+++ server/effects/hue_rotation.py
@@ -0,0 +1,14 @@
+import colorsys
+import numpy as np
+
+def apply_hue_rotation(img, params=None):
+    angle = params.get('angle', 0)
+    img = img.convert('RGB')
+    np_img = np.array(img) / 255.0
+    r, g, b = np_img[..., 0], np_img[..., 1], np_img[..., 2]
+    h, s, v = np.vectorize(colorsys.rgb_to_hsv)(r, g, b)
+    h = (h + angle / 360.0) % 1.0
+    r, g, b = np.vectorize(colorsys.hsv_to_rgb)(h, s, v)
+    np_img = np.stack((r, g, b), axis=-1)
+    np_img = (np_img * 255).astype(np.uint8)
+    return Image.fromarray(np_img)
